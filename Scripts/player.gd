8f12722fe41d9aa0bfdab36e264cb5885da26a3f
Robert Henderson <grendark@live.com>
2024-03-05 13:03:12 -0600

    play movement updated
    still needs tweaks

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,28 +1,60 @@
 extends CharacterBody2D
 
 
-const SPEED = 275.0
-const JUMP_VELOCITY = -375.0
+const speed = 500.0
+const jump_power = -2000.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+#var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+const acceleration = 50
+const friction = 70
+@export var fallMultiplier = 10
+@export var lowJumpMultiplier = 10 
+@export var jumpVelocity = 1000 #Jump height
 
-
+#Physics
+@export var gravity = 8
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
-
-	# Handle jump.
-	if Input.is_action_just_pressed("ui_up") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
+	var input_dir: Vector2 = input()
+	if input_dir != Vector2.ZERO:
+		accelerate(input_dir)
+		#play_animation()
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
+		add_friction()
+		#idle animation
+	
+	
+	#gravity
+	jump()
+	player_movement()
+	print(velocity)
+func input() -> Vector2:
+	var input_dir = Vector2.ZERO
+	
+	input_dir.x = Input.get_axis("ui_left","ui_right")
+	return input_dir.normalized()
+func  accelerate(direction):
+	velocity = velocity.move_toward(speed * direction, acceleration)
+	
+func add_friction():
+	velocity = velocity.move_toward(Vector2.ZERO,friction)
+	
+func player_movement():
 	move_and_slide()
+func play_animation():
+	pass
+func jump():
+	#Applying gravity to player
+	velocity.y += gravity 
+	
+	#Jump Physics
+	if velocity.y > 0: #Player is falling
+		velocity += Vector2.UP * (-9.81) * (fallMultiplier) #Falling action is faster than jumping action | Like in mario
+
+	elif velocity.y < 0 && Input.is_action_just_released("ui_up"): #Player is jumping 
+		velocity += Vector2.UP * (-9.81) * (lowJumpMultiplier) #Jump Height depends on how long you will hold key
+
+	if is_on_floor():
+		if Input.is_action_just_pressed("ui_up"): 
+			velocity = Vector2.UP * jumpVelocity #Normal Jump action
+				
